c5492587169dd3744e78b66e90cf5caac8ad8e8b
Caleb Larson <cllarson@umass.edu>
2015-04-26 17:43:56 -0400

    Added geolocation js file to obtain user longitude and latutude.

---
+++ src/app/public/javascripts/geolocation.js
@@ -0,0 +1,23 @@
+var latitude = 0;
+var longitude = 0;
+
+window.addEventListener("DOMContentLoaded", function() {
+
+	document.getElementById("location").onsubmit = function(e) {
+		getLocation();
+		document.getElementById("latitude").value = latitude;
+		document.getElementById("longitude").value = longitude;
+	};
+});
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(getCoordinates);
+    } else { 
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+function getCoordinates(position) {
+	latitude = position.coords.latitude;
+	longitude = position.coords.longitude;
+}
