81a7632afb0c8adce1076c9a6b33c25d2b3fd3e1
Bruce Spang <bruce@brucespang.com>
2015-04-26 15:13:57 -0400

    Validate questions

--- src/api/lib/modules/questions/models.py
+++ src/api/lib/modules/questions/models.py
@@ -1,11 +1,25 @@
 import uuid
-from app import db
+from app import db, ApiError
 from datetime import datetime
+from sqlalchemy.orm import validates
 
 class Question(db.Model):
     __tablename__ = 'questions'
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.Text(), nullable=False)
+
+    @validates("question")
+    def validate_question(self, key, question):
+        if not question:
+            raise ApiError("Missing question")
+
+        if len(question) <= 1:
+            raise ApiError("Question is too short")
+
+        if question[-1] != "?":
+            raise ApiError("Question doesn't end with a question mark")
+
+        return question
 
     def __init__(self, question):
         self.id = str(uuid.uuid1())
